814c5d88effdfe38979ff5810bea1c5d3570d11c
Marcelo Trier <marcelo.trier@gmail.com>
2014-07-10 15:34:14 -0300

    Pequenas mudanças.

--- src/br/compstring/Principal.java
+++ src/br/compstring/Principal.java
@@ -1,5 +1,7 @@
 package br.compstring;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -7,10 +9,17 @@ public class Principal {
 //		String B = "ontologico";
 		String A = "instituto";
 		String B = "instituição";
+		Scanner teclado;
+		teclado = new Scanner( System.in );
+		System.out.print( "String A: " );
+		A = teclado.nextLine();
+		System.out.print( "String B: " );
+		B = teclado.nextLine();
 		CompString cs = new CompString();
 		cs.init( A, B );
 		cs.execute();
 		System.out.println( cs );
+		teclado.close();
 	}
 
 }
